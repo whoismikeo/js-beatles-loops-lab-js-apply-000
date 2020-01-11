ed1112a1bb6e877fd160e9260645823db311d2e1
Michael Ochnicki <ochnicki.michael@gmail.com>
2020-01-11 03:42:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  let arr = [];
+  
+  for (var i = 0; i < musicians.length; i++) {
+    arr.push(musicians[i] + ' plays ' + instruments[i]);
+  }
+  
+  return arr;
+}
